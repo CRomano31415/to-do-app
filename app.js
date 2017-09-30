f1cca05afccc5cb9f1746c6abb1e63d817d31535
Claudia Romano <cromano31415@gmail.com>
2017-09-29 22:26:49 -0500

    final version

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
 function onReady(){
-	const addToDoForm = document.getElementByID('addToDoForm');
-	const newToDoText = document.getElementByID('newToDoText');
-	const toDoList = document.getElementByID('toDoList');
+	const addToDoForm = document.getElementById('addToDoForm');
+	const newToDoText = document.getElementById('newToDoText');
+	const toDoList = document.getElementById('toDoList');
+
 
 	addToDoForm.addEventListener('submit', () => {
 		event.preventDefault();
 
+		//if the button clicked was Add, then add
+		//otherwise if it was Delete, then delete
+
 		let title = newToDoText.value;
 
-		let newLi = document.createElement('li');
 		let checkbox = document.createElement('input');
+		let button = document.createElement('button');
+		let del = document.createTextNode('Nevermind, Delete This');
+		let newLi = document.createElement('li');
 
 		checkbox.type = "checkbox";
+		button.value = "delete";
 
 		newLi.textContent = title;
 
 		toDoList.appendChild(newLi);
 		newLi.appendChild(checkbox);
+		newLi.appendChild(button);
+		button.appendChild(del);
 
 		newToDoText.value = '';
 
+		button.addEventListener('click', () =>{
+			toDoList.removeChild(newLi);
+
+		});
 	});
+
 }	
 
 window.onload = function(){
 	onReady();
 };
+
