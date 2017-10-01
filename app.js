d796d046c9da51f41529656c6d64fc44330f4a60
Claudia Romano <cromano31415@gmail.com>
2017-10-01 14:47:38 -0500

    Checkpoint-8-events-2 completed

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
 function onReady(){
-	const addToDoForm = document.getElementById('addToDoForm');
-	const newToDoText = document.getElementById('newToDoText');
-	const toDoList = document.getElementById('toDoList');
+	var toDos = [];
+	var addToDoForm = document.getElementById('addToDoForm');
 
-
-	addToDoForm.addEventListener('submit', () => {
-		event.preventDefault();
-
-		//if the button clicked was Add, then add
-		//otherwise if it was Delete, then delete
-
-		let title = newToDoText.value;
-
-		let checkbox = document.createElement('input');
-		let button = document.createElement('button');
-		let del = document.createTextNode('Nevermind, Delete This');
-		let newLi = document.createElement('li');
-
-		checkbox.type = "checkbox";
-		button.value = "delete";
-
-		newLi.textContent = title;
-
-		toDoList.appendChild(newLi);
-		newLi.appendChild(checkbox);
-		newLi.appendChild(button);
-		button.appendChild(del);
-
+	function createNewToDo(){
+		var newToDoText = document.getElementById('newToDoText');
+		toDos.push({
+			title: newToDoText.value,
+			complete: false
+		});
 		newToDoText.value = '';
 
-		button.addEventListener('click', () =>{
-			toDoList.removeChild(newLi);
+		renderTheUI(toDos);
+	}
 
+	function renderTheUI(toDos){
+		var todoList = document.getElementById('toDoList');
+
+		toDoList.innerHTML = '';
+
+		toDos.forEach(function(toDo){
+			var newLi = document.createElement('li');
+			var checkbox = document.createElement('input');
+			checkbox.type = "checkbox";
+
+			newLi.innerHTML = toDo.title;
+
+			todoList.appendChild(newLi);
+			newLi.appendChild(checkbox);
 		});
+
+	}
+
+	addToDoForm.addEventListener('submit', function(event){
+		event.preventDefault();
+		createNewToDo();
 	});
 
-}	
+	renderTheUI(toDos);
+}
+
 
 window.onload = function(){
 	onReady();
